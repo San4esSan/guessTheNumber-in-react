0708bb38cfe90f0bddb9cccee771ce7228477fa4
mister.San4es <mister.San4es@list.ru>
2022-07-14 17:28:56 +0400

    start

---
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import style from './ClassComponent.module.css';
+
+export class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 5
+    };
+  }
+  render() {
+    return (
+      <div className={style.game}>
+        <p className={style.result}>5</p>
+        <form className={style.form}>
+          <label className={style.label} htmlFor='user_number'>
+            Угадай число
+          </label>
+          <input className={style.input} type='number' id='user_number' />
+          <button className={style.btn}>Угадать</button>
+        </form>
+      </div>
+    );
+  }
+}
